757e3839a6375fd98987fd7f9aa586bc789a9b22
Copser <iamrmot@gmail.com>
2015-10-26 20:09:28 +0100

    Napravljen static i template path u settings.py, dodat htmlbootstrap za BookClaw

--- claw_main/claw_main/settings.py
+++ claw_main/claw_main/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'claw_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,7 +100,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
