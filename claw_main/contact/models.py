18da7c459444240bdf68c4367121d24d10dde304
Copser <iamrmot@gmail.com>
2015-10-27 21:02:57 +0100

    Definisan ContactForm u contact/models.py, izvrsena migracija, update databaza

--- claw_main/contact/models.py
+++ claw_main/contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
+class ContactForm(models.Model):
+
+    """Docstring for ContactForm. """
+    name = models.CharField(max_length=150)
+    email = models.CharField(max_length=250)
+    topic = models.CharField(max_length=150)
+    message = models.CharField(max_length=1000)
+    timestamp = models.DateTimeField(default=datetime.datetime.now)
+
+    def __unicode__(self):
+        return self.email
+
+    class Meta:
+        ordering = ['-timestamp']
