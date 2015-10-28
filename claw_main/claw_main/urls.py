c2198d54886ed12a1a6d926405881749dce6874a
Copser <iamrmot@gmail.com>
2015-10-28 20:32:12 +0100

    Kontakt app napreduje, napravljen views.py, dodat urls.py u main app

--- claw_main/claw_main/urls.py
+++ claw_main/claw_main/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'main.views.index', name='home'),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
+    url(r'^contact/', 'contact.views.contact', name='contact'),
 ]
