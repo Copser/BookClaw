bde3a4caf86e8bce22cd2d354115ddbceabf463f
Copser <iamrmot@gmail.com>
2015-11-25 06:51:49 +0100

    Namestam STATIC_ROOT za heroku deploy

--- claw_main/claw_main/settings.py
+++ claw_main/claw_main/settings.py
@@ -140,7 +140,7 @@ except Exception, e:
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
