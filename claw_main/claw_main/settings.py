c0a9591008c336b9cbd70ac70ee168c2342c4495
Copser <iamrmot@gmail.com>
2015-12-08 06:56:16 +0100

    ls

--- claw_main/claw_main/settings.py
+++ claw_main/claw_main/settings.py
@@ -142,10 +142,10 @@ except Exception, e:
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Configuration S3 Amazon Web Service for static serving
 # Storage on S3 settings are stored as os.environs to keep settings.py clean
